c557a07d1f33fae29b0d08086f64a3964e7f1d3c
EgaAprianto <egaaprianto04@gmail.com>
2023-11-21 14:26:26 +0700

    [add] stage 1 & 2

--- Scene/Player/Player.gd
+++ Scene/Player/Player.gd
@@ -3,10 +3,12 @@ extends CharacterBody2D
 @onready var animation_tree = $AnimationTree
 @onready var sprite_2d = $Sprite2D
 @onready var state_machine = $PlayerStateMachine
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 @export var speed = 300.0
 @export var double_jump_velocity = 375.0
 @export var wall_slide_velocity = 200.0
+@export var jump_velocity = 425.0
 
 const MAX_JUMPS = 2
 
@@ -39,23 +41,23 @@ func jump_gravity(delta):
 	velocity.y += gravity * delta
 	
 	# Pergerakan Karakter Melompat dan Animasi
-##	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
-#	var is_double_jumping = Input.is_action_just_pressed("jump")
-#	var is_falling = velocity.y > 0.0 and not is_on_floor()
-#	if is_jumping:
-##		animated_sprite_2d.animation = "jump"
-#		jump_made += 1
-#		velocity.y = -jump_velocity
-#	elif is_double_jumping:
-#		jump_made += 1
-#		if jump_made == MAX_JUMPS:
-#			velocity.y = -double_jump_velocity
-##			animated_sprite_2d.animation = "double_jump"
-#	elif is_falling:
-#		pass
-##		animated_sprite_2d.animation = "fall"
-#	elif is_on_floor():
-#		jump_made = 0
+	var is_jumping = Input.is_action_just_pressed("jump") and is_on_floor()
+	var is_double_jumping = Input.is_action_just_pressed("jump")
+	var is_falling = velocity.y > 0.0 and not is_on_floor()
+	if is_jumping:
+		animated_sprite_2d.animation = "jump"
+		jump_made += 1
+		velocity.y = -jump_velocity
+	elif is_double_jumping:
+		jump_made += 1
+		if jump_made == MAX_JUMPS:
+			velocity.y = -double_jump_velocity
+			animated_sprite_2d.animation = "double_jump"
+	elif is_falling:
+		pass
+		animated_sprite_2d.animation = "fall"
+	elif is_on_floor():
+		jump_made = 0
 
 
 func _physics_process(delta):
